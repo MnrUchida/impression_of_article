2832fed9fdeadf7a4f7e6412ad026fe3f5e9ad0c
MnrUchida <mukenan108@gmail.com>
2022-12-28 10:53:27 +0900

    踊り手別の一覧を追加
    close #57

--- app/models/article.rb
+++ app/models/article.rb
@@ -43,6 +43,17 @@ class Article < ApplicationRecord
     SQL
     where(sanitize_sql_array([sql, { name: "%#{name}%" }]))
   }
+  scope :has_published_impressions, lambda {
+    sql = <<~SQL
+      EXISTS (
+            SELECT 1 
+              FROM impressions
+             WHERE impressions.article_id = articles.id
+               AND impressions.status = :status
+      )
+    SQL
+    where(sanitize_sql_array([sql, { status: Impression.statuses[:published] }]))
+  }
 
   def self.find_or_initialize_by_url(url)
     article = _find_by_url(url)
