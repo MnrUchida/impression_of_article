b507150eb7b637c7b51868cbffef619d7e4af177
MnrUchida <mukenan108@gmail.com>
2023-01-01 16:06:11 +0900

    楽曲の選択をURLで行えるようにする
    close #90

--- app/models/concerns/url_parser.rb
+++ app/models/concerns/url_parser.rb
@@ -4,6 +4,14 @@ module UrlParser
   require "open-uri"
 
   module ClassMethods
+    def _by_url(url)
+      html = URI.open(url).read
+      parsed = Nokogiri.parse(html)
+      adjusted_url = parsed.css('meta[property="og:url"]').attribute('content').value
+      self.where(url: adjusted_url)
+    rescue => e
+    end
+
     def _find_or_initialize_by_url(url)
       html = URI.open(url).read
       parsed = Nokogiri.parse(html)
