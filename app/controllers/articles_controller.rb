43f9d48bdfba8e4d60143979b42a3f3a4a17739b
MnrUchida <mukenan108@gmail.com>
2023-01-16 19:18:12 +0900

    twitter用のコピーにタグを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,4 @@ class ArticlesController < ApplicationController
     @article = Article.has_published_impressions.find(params[:id])
     send_data(@article.image_data, { filename: "image_data_#{@article.id}", type: @article.content_type, disposition: :inline })
   end
-
 end
